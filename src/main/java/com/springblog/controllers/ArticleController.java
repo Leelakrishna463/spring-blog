d61ab34a0ca1435d4b2fcb05e404eb5fc7b40d8c
Abhishek Shandilya <abhishek.shandilya@decathlon.com>
2024-02-07 22:19:52 +0530

    Fixed issue with controller

--- src/main/java/com/springblog/controllers/ArticleController.java
+++ src/main/java/com/springblog/controllers/ArticleController.java
@@ -3,13 +3,13 @@ package com.springblog.controllers;
 import com.springblog.models.Article;
 import com.springblog.service.ArticleService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/article")
 public class ArticleController {
