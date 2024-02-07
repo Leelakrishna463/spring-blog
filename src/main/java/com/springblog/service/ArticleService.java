dfaf55dccb14f62093777439862fb5336517da8b
Abhishek Shandilya <abhishek.shandilya@decathlon.com>
2024-02-07 21:53:58 +0530

    Added ArticleController and updated service to use repository

--- src/main/java/com/springblog/service/ArticleService.java
+++ src/main/java/com/springblog/service/ArticleService.java
@@ -1,14 +1,19 @@
 package com.springblog.service;
 
 import com.springblog.models.Article;
+import com.springblog.repository.ArticleRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class ArticleService {
 
-    public List<Article> getAllArticles() {
+    private final ArticleRepository articleRepository;
 
+    public List<Article> getAllArticles() {
+        return articleRepository.findAll();
     }
 }
